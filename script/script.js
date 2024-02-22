cc4e2235eb1dbf4d92ff3dee85a0b1b6330f0fa9
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2024-02-22 08:01:22 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     
 })
+function updateCurrentTime() {
+    var currentTimeElement = document.getElementById('currentTime');
+    var currentTime = new Date();
+    
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    
+    var formattedTime = hours + ":" + minutes + ":" + seconds;
+    
+    currentTimeElement.innerHTML = "Current Time: " + formattedTime;
+}
+
+updateCurrentTime();
+
+setInterval(updateCurrentTime, 1000);
